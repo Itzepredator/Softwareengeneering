85cda8dafa0318dd861cc34de1d7b554a50b0dce
Itzepredator <Heinen-andreas@web.de>
2013-10-30 00:00:05 +0100

    Cucumber erweitert um Suche und Feedback

---
+++ Softwareengeneering/src/test/java/github/com/Itzepredator/Softwareengeneering/FeedbackSteps.java
@@ -0,0 +1,74 @@
+package github.com.Itzepredator.Softwareengeneering;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.de.Wenn;
+import cucumber.api.java.de.Dann;
+import cucumber.api.java.de.Angenommen;
+
+public class FeedbackSteps {
+	@Angenommen("^das Formular abgesendet hat,$")
+	public void das_Formular_abgesendet_hat() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die eingegebenen Daten Validiert.$")
+	public void werden_die_eingegebenen_Daten_Validiert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Validierung abgeschlossen ist,$")
+	public void die_Validierung_abgeschlossen_ist() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die Feedbackdaten in der Datenbank gespeichert.$")
+	public void werden_die_Feedbackdaten_in_der_Datenbank_gespeichert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^die Daten können in der Rubrik Feedback angezeigt werden.$")
+	public void die_Daten_können_in_der_Rubrik_Feedback_angezeigt_werden() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^bei der Validierung ein Fehler aufgetreten ist \\(z.B. Aufgrund nicht erlaubter Zeichen\\),$")
+	public void bei_der_Validierung_ein_Fehler_aufgetreten_ist_z_B_Aufgrund_nicht_erlaubter_Zeichen_() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^sein Feedback anpassen$")
+	public void sein_Feedback_anpassen() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^erneut absenden.$")
+	public void erneut_absenden() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^es beim Speichern der Daten zu einem Fehler kommt$")
+	public void es_beim_Speichern_der_Daten_zu_einem_Fehler_kommt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^bekommt der Benutzer eine Fehlermeldung angezeigt$")
+	public void bekommt_der_Benutzer_eine_Fehlermeldung_angezeigt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der aktuelle Flow wird unterbrochen, jedoch nicht der komplette UseCase beendet.$")
+	public void der_aktuelle_Flow_wird_unterbrochen_jedoch_nicht_der_komplette_UseCase_beendet() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+}
---
+++ Softwareengeneering/src/test/java/github/com/Itzepredator/Softwareengeneering/SucheSteps.java
@@ -0,0 +1,82 @@
+package github.com.Itzepredator.Softwareengeneering;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.de.Wenn;
+import cucumber.api.java.de.Dann;
+import cucumber.api.java.de.Angenommen;
+
+public class SucheSteps {
+
+	@Angenommen("^seine Anfrage mittels dem Suchen-Button abgesendet hat.$")
+	public void seine_Anfrage_mittels_dem_Suchen_Button_abgesendet_hat() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die Filterkriterien validiert.$")
+	public void werden_die_Filterkriterien_validiert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Filterkriterien validiert werden konnten,$")
+	public void die_Filterkriterien_validiert_werden_konnten() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^Werden die für den Benutzer entsprechenden Daten aus der Datenbank geladen.$")
+	public void Werden_die_für_den_Benutzer_entsprechenden_Daten_aus_der_Datenbank_geladen() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die für den Benutzer passenden Daten aus der Datenbank geladen werden konnten.$")
+	public void die_für_den_Benutzer_passenden_Daten_aus_der_Datenbank_geladen_werden_konnten() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden dem Benutzer die Daten auf der Hompage/App angezeigt.$")
+	public void werden_dem_Benutzer_die_Daten_auf_der_Hompage_App_angezeigt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Filterkriterien nicht validiert werden konnten,$")
+	public void die_Filterkriterien_nicht_validiert_werden_konnten() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^wird dem Benutzer eine Fehlermeldung ausgegeben$")
+	public void wird_dem_Benutzer_eine_Fehlermeldung_ausgegeben() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der Benutzer kann die Daten der Abfrage ändern$")
+	public void der_Benutzer_kann_die_Daten_der_Abfrage_ändern() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^eine erneute Abfrage starten.$")
+	public void eine_erneute_Abfrage_starten() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^beim laden der Daten aus der Datenbank ein Fehler auftritt,$")
+	public void beim_laden_der_Daten_aus_der_Datenbank_ein_Fehler_auftritt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der Aktuelle Flow wird beendet aber nicht der komplette UseCase$")
+	public void der_Aktuelle_Flow_wird_beendet_aber_nicht_der_komplette_UseCase() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+}

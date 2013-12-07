31e658569c7d64b90e33c8ed9f6c3d257ad740e7
Itzepredator <Heinen-andreas@web.de>
2013-12-07 23:22:49 +0100

    Aktuellste version von Cucumber

---
+++ src/main/java/github/com/Itzepredator/Softwareengeneering/BenutzerAnlegenSteps.java
@@ -0,0 +1,95 @@
+package github.com.Itzepredator.Softwareengeneering;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.de.Wenn;
+import cucumber.api.java.de.Dann;
+import cucumber.api.java.de.Angenommen;
+
+public class BenutzerAnlegenSteps {
+
+
+	@Angenommen("^diese absendet.$")
+	public void diese_absendet() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die PersÃ¶nlichen Informationen validiert.$")
+	public void werden_die_Persönlichen_Informationen_validiert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Validierung in Ordnung war,$")
+	public void die_Validierung_in_Ordnung_war() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die Informationen in der Datenbank gespeichert.$")
+	public void werden_die_Informationen_in_der_Datenbank_gespeichert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Informationen gespeichert wurden,$")
+	public void die_Informationen_gespeichert_wurden() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^bekommt der Benutzer eine BestÃ¤tigung, dass der Vorgang erfolgreich abgeschlossen wurde$")
+	public void bekommt_der_Benutzer_eine_BestÃ¤tigung_dass_der_Vorgang_erfolgreich_abgeschlossen_wurde() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der Benutzer angelegt wurde.$")
+	public void der_Benutzer_angelegt_wurde() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^werden die Informationen validiert.$")
+	public void werden_die_Informationen_validiert() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^beim validieren der  Persönlichen Informationen ein Fehler auftritt,$")
+	public void beim_validieren_der_Persönlichen_Informationen_ein_Fehler_auftritt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der Vorgang wird unterbrochen.$")
+	public void der_Vorgang_wird_unterbrochen() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^hat der Benutzer die Möglichkeit seine Eingabe zu korrigieren.$")
+	public void hat_der_Benutzer_die_Möglichkeit_seine_Eingabe_zu_korrigieren() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^die Persönlichen Informationen validiert sind,$")
+	public void die_Persönlichen_Informationen_validiert_sind() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Wenn("^beim Speichern in die Datenbank ein Fehler auftritt,$")
+	public void beim_Speichern_in_die_Datenbank_ein_Fehler_auftritt() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Dann("^der Aktuelle \"([^\"]*)\" wird unterbrochen, jedoch nicht der komplette UseCase.$")
+	public void der_Aktuelle_wird_unterbrochen_jedoch_nicht_der_komplette_UseCase(String arg1) throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+}
---
+++ src/test/resources/Feedback.feature
@@ -0,0 +1,32 @@
+# language: de
+
+Funktionalität: Feedback
+	als ein Benutzer
+	um von Ihm besuchte Tankstellen zu Bewerten
+	Der Benutzer möchte eine Tankstelle bewerten.
+	
+Szenario:(Mainflow) Wenn Der Benutzer Informationen in das Feedbackformular eingetragen hat
+					Und das Formular abgesendet hat,
+					Dann werden die eingegebenen Daten Validiert.
+					Wenn die Validierung abgeschlossen ist,
+					Dann werden die Feedbackdaten in der Datenbank gespeichert.
+					Und die Daten können in der Rubrik Feedback angezeigt werden.
+					
+Szenario:(Alternative 1) Wenn Der Benutzer Informationen in das Feedbackformular eingetragen hat
+					Und das Formular abgesendet hat,
+					Dann werden die eingegebenen Daten Validiert.
+					Wenn bei der Validierung ein Fehler aufgetreten ist (z.B. Aufgrund nicht erlaubter Zeichen),
+					Dann bekommt der Benutzer eine Fehlermeldung 
+					Und sein Feedback anpassen 
+					Und erneut absenden.
+					
+Szenario:(Alternative 2) Wenn Der Benutzer Informationen in das Feedbackformular eingetragen hat
+					Und das Formular abgesendet hat,
+					Dann werden die eingegebenen Daten Validiert.
+					Wenn die Validierung abgeschlossen ist,
+					Dann werden die Feedbackdaten in der Datenbank gespeichert.
+					Wenn es beim Speichern der Daten zu einem Fehler kommt
+					Dann bekommt der Benutzer eine Fehlermeldung angezeigt
+					Und der aktuelle Flow wird unterbrochen, jedoch nicht der komplette UseCase beendet. 
+																				
+					
